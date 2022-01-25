fc4cf8739ee0a77fc83b5232070e4f39702a917b
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-25 21:10:06 +0700

    update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,22 @@
-const { User } = require("../models");
+const { User, Ticket, Comment } = require("../models");
 const slug = require('slug');
 const bcrypt = require("bcrypt");
 const { configEnv } = require("../config/server.config");
 const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+
+const userAttributes = [
+    "id",
+    "name",
+    "phoneNumber",
+    "email",
+    "alias",
+    "avatar",
+    "roles",
+    "dateOfBirth",
+    "createdAt",
+    "updatedAt",
+];
 const createUser = async (req, res) => {
     try {
         const {
@@ -55,6 +69,215 @@ const uploadAvatar = async (req, res) => {
     }
 };
 
+const getListUser = async (req, res) => {
+    try {
+        const userResult = await User.findAll({
+            attributes: userAttributes
+        });
+        res.status(200).send(userResult);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const getDetailUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userResult = await User.findOne({
+            where: {
+                id
+            },
+            include: [
+                {
+                    model: Ticket
+                }
+            ],
+            attributes: userAttributes
+        });
+        res.status(200).send(userResult);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+const uploadAvatarAdmin = async (req, res) => {
+    try {
+        const { resultImage } = req;
+        const { id } = req.params;
+        await User.update(
+            { avatar: resultImage.url },
+            {
+                where: {
+                    id
+                },
+            }
+        );
+        const userPK = await User.findByPk(id);
+        const userResult = { ...userPK.dataValues };
+        delete userResult.password;
+
+        res.status(201).send(userResult);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const getMyInfo = async (req, res) => {
+    try {
+        const { user } = req;
+        const userResult = await User.findOne({
+            where: {
+                id: user.id
+            },
+            include: [
+                {
+                    model: Ticket
+                }
+            ],
+            attributes: userAttributes
+        });
+        res.status(200).send(userResult);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Ticket.destroy({
+            where: {
+                userId: id
+            }
+        });
+        await Comment.destroy({
+            where: {
+                userId: id
+            }
+        });
+        await User.destroy({
+            where: {
+                id
+            }
+        });
+
+        res.send({ message: "delete successfully" });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {
+            name,
+            phoneNumber,
+            dateOfBirth,
+            roles
+        } = req.body;
+        const alias = slug(name);
+
+        await User.update(
+            {
+                name,
+                phoneNumber,
+                dateOfBirth,
+                roles: roles === "ADMIN" || roles === "CLIENT" ? roles : "CLIENT",
+                alias,
+            },
+            {
+                where: {
+                    id
+                },
+            }
+        );
+
+        res.send({ message: "update user successfully" });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const updateInfo = async (req, res) => {
+    try {
+        const { id } = req.user;
+        const {
+            name,
+            phoneNumber,
+            dateOfBirth,
+        } = req.body;
+        const alias = slug(name);
+
+        await User.update(
+            {
+                name,
+                phoneNumber,
+                dateOfBirth,
+                alias,
+            },
+            {
+                where: {
+                    id
+                },
+            }
+        );
+
+        res.send({ message: "update user successfully" });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const searchUser = async (req, res) => {
+    try {
+        const { keyword } = req.query;
+
+        const listUser = await User.findAll(
+            {
+                where: {
+                    name: {
+                        [Op.substring]: keyword
+                    }
+                }
+            }
+        );
+
+        res.send(listUser);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const getUserPagination = async (req, res) => {
+    try {
+        const { pageSize, pageCurrent } = req.query;
+
+        const { count, rows } = await User.findAndCountAll({
+            offset: (+pageCurrent - 1) * pageSize,
+            limit: +pageSize
+        });
+        const totalPage = Math.ceil(count / pageSize);
+
+        res.send({ totalPage, rows });
+
+    } catch (error) {
+        console.log(totalItem);
+        res.status(500).send(error);
+
+    }
+};
+
 module.exports = {
-    createUser, uploadAvatar
+    createUser,
+    uploadAvatar,
+    getListUser,
+    getDetailUserById,
+    uploadAvatarAdmin,
+    getMyInfo,
+    deleteUser,
+    updateUser,
+    updateInfo,
+    searchUser,
+    getUserPagination
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,23 @@
 const { Router } = require("express");
-
+const { User } = require("../models");
 const { uploadImage } = require("../controllers/upload.controllers");
-const { createUser, uploadAvatar } = require("../controllers/user.controllers");
+const { createUser, uploadAvatar, getListUser, getDetailUserById, uploadAvatarAdmin, getMyInfo, deleteUser, updateUser, updateInfo, searchUser, getUserPagination } = require("../controllers/user.controllers");
 const { authenticate, authorize } = require("../middlewares/auth/auth.middleware");
 const { uploadImageSingle } = require("../middlewares/uploads/upload-image.middleware");
+const { checkExist } = require("../middlewares/validations/check-exist.middlewares");
 
 const userRouter = Router();
 
 userRouter.post("/", authenticate, authorize(["ADMIN"]), createUser);
+userRouter.get("/", authenticate, authorize(["ADMIN"]), getListUser);
+userRouter.get("/my-info", authenticate, getMyInfo);
+userRouter.get("/pagination", authenticate, authorize(["ADMIN"]), getUserPagination);
 userRouter.post("/upload-avatar", authenticate, uploadImageSingle().single("image"), uploadImage("Avatar"), uploadAvatar);
+userRouter.get("/search", authenticate, authorize(["ADMIN"]), searchUser);
+userRouter.post("/upload-avatar/:id", authenticate, authorize(["ADMIN"]), uploadImageSingle().single("image"), uploadImage("Avatar"), uploadAvatarAdmin);
+userRouter.get("/:id", authenticate, authorize(["ADMIN"]), checkExist(User), getDetailUserById);
+userRouter.delete("/:id", authenticate, authorize(["ADMIN"]), checkExist(User), deleteUser);
+userRouter.put("/", authenticate, updateInfo);
+userRouter.put("/:id", authenticate, authorize(["ADMIN"]), checkExist(User), updateUser);
+
 module.exports = { userRouter };
