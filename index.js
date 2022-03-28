165bba0b571e18337776ef5da4f9549c5d03f374
dovanthao2404 <dovanthao2404@gmail.com>
2022-03-28 21:34:17 +0700

    add movie duration

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const cors = require("cors");
 const { rootRouter } = require("./src/routes");
 const { configEnv } = require("./src/config/server.config");
 
+const http = require('http');
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 dotenv.config();
+app.use(cors());
 
 app.use(express.json());
+io.on('connection', (socket) => {
+});
 
-app.use(cors());
+
+
 
 app.use("/api/v1", rootRouter);
 
 const PORT = process.env.PORT || configEnv.server.port;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("app listen port " + PORT);
 });
